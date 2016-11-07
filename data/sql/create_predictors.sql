3a733ea67f7c151218c15a3cc7c254bd6af490c6
aberegov <alekseyberegov@gmail.com>
2016-11-07 10:31:32 -0600

    Updated test case

---
+++ data/sql/create_predictors.sql
@@ -0,0 +1,21 @@
+--
+-- Create viewability predictors table
+--
+CREATE TABLE
+    viewability_predictors
+    (
+        vendor_id INTEGER NOT NULL,
+        network_id INTEGER,
+        seller_id CHARACTER VARYING(100),
+        site_id TEXT,
+        ad_format_id INTEGER,
+        media_size INTEGER,
+        ad_position INTEGER,
+        browser_name CHARACTER VARYING(100),
+        browser_version CHARACTER VARYING(100),
+        os CHARACTER VARYING(100),
+        device CHARACTER VARYING(100),
+        predictor_type CHARACTER VARYING(100) NOT NULL,
+        predictor_value NUMERIC(12,5),
+        confidence_score NUMERIC(12,5)
+    );
