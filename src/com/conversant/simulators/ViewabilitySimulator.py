dbb0d721bbe85b62887100238f36e2d174dd5402
beregov <admin@myipvoice.com>
2016-11-08 21:37:28 -0600

    Added node support

--- src/com/conversant/simulators/ViewabilitySimulator.py
+++ src/com/conversant/simulators/ViewabilitySimulator.py
@@ -1,7 +1,11 @@
+import logging
+from matplotlib import pyplot
 from com.conversant.viewability.MultiKeyPredictor import MultiKeyPredictor
 from com.conversant.viewability.ViewabilityController import ViewabilityController
 from com.conversant.common.SQLShell import SQLShell
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ViewabilitySimulator:
     def __init__(self, goal, n=100000, w=10000, l=10000,source='ad_quality.impressions_view'):
@@ -20,10 +24,14 @@ class ViewabilitySimulator:
             'ad_position'
         ]
 
+        logger.info('Start building multi-key lookup tree')
         self.predictor.build()
+
         self.controller = ViewabilityController(predictor=self.predictor, goal=goal, n=n, w=w, l=l)
+        self.results = {'rate': [], 'threshold': []}
 
     def run(self):
+        logger.info('Start running simulation')
         data = SQLShell()
         data.execute("SELECT {0}, in_view, measured FROM {1}".format
                      (str(self.predictor.multi_key), self.source), {}, self.handle_impression)
@@ -32,8 +40,18 @@ class ViewabilitySimulator:
         self.controller.process_event(imp, self.output)
 
     def output(self, data):
-        print(data)
+        logger.info(data)
+        self.results['threshold'].append(data[0])
+        self.results['rate'].append(data[2])
+
+    def show_results(self):
+        logger.info('Displaying results')
+        x = list(range(len(self.results['rate'])))
+        pyplot.plot(x, self.results['rate'], color='green', linestyle='solid')
+        pyplot.show()
+
 
 if __name__ == '__main__':
-    simulator = ViewabilitySimulator(goal=0.7, n=1000, w=100, l=100)
+    simulator = ViewabilitySimulator(goal=0.7, n=10000, w=100, l=100)
     simulator.run()
+    simulator.show_results()
