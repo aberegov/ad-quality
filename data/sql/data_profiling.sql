c091cd2864dbaebc8d722ba0ac92a51c28c4cb21
aberegov <alekseyberegov@gmail.com>
2016-11-10 15:59:27 -0600

    Added predictor

--- data/sql/data_profiling.sql
+++ data/sql/data_profiling.sql
@@ -12,3 +12,26 @@ group by 1
 --
 select count(1)
 from ad_quality.impressions_view
+
+--
+-- NULLs and empty strings
+--
+select 'predictors' as stats, seller_id_null, seller_id_empty, site_id_null, site_id_empty
+from
+    (select count(1) as seller_id_null from ad_quality.predictors where seller_id is null) a
+    cross join
+    (select count(1) as seller_id_empty from ad_quality.predictors where seller_id = '') b
+    cross join
+    (select count(1) as site_id_null from ad_quality.predictors where site_id is null) c
+    cross join
+    (select count(1) as site_id_empty from ad_quality.predictors where site_id = '') d
+    union
+select 'impressions' as stats, seller_id_null, seller_id_empty, site_id_null, site_id_empty
+from
+    (select count(1) as seller_id_null from ad_quality.impressions_view where seller_id is null) a
+    cross join
+    (select count(1) as seller_id_empty from ad_quality.impressions_view where seller_id = '') b
+    cross join
+    (select count(1) as site_id_null from ad_quality.impressions_view where site_id is null) c
+    cross join
+    (select count(1) as site_id_empty from ad_quality.impressions_view where site_id = '') d
