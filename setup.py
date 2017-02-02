7cbe0b65c82ccaaa767a6b7a6070e213e06ee877
Aleksey Beregov <alekseyberegov@gmail.com>
2017-02-02 17:15:19 -0600

    Added ci

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='ad-quality',
     version='1.0.0',
-    packages=['', 'com', 'com.conversant', 'com.conversant.common', 'com.conversant.simulators',
+    packages=['com', 'com.conversant', 'com.conversant.common', 'com.conversant.simulators',
               'com.conversant.viewability', 'unit', 'unit.conversant', 'unit.conversant.common',
               'unit.conversant.viewability'],
-    package_dir={'test': 'src'},
+    package_dir={'': 'src'},
     url='',
     license='',
     author='beregov',
