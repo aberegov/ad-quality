e8e42bc64a9201d4184315b61e1c1ccc72e8441e
Aleksey Beregov <alekseyberegov@gmail.com>
2017-02-02 17:01:41 -0600

    Added ci

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ad-quality',
+    version='1.0.0',
+    packages=['', 'com', 'com.conversant', 'com.conversant.common', 'com.conversant.simulators',
+              'com.conversant.viewability', 'unit', 'unit.conversant', 'unit.conversant.common',
+              'unit.conversant.viewability'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='beregov',
+    author_email='alekseyberegov@gmail.com',
+    description=''
+)
