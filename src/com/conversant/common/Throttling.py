e06e0bb9646fc248d6ccf35c0471e44a72618fee
Aleksey Beregov <alekseyberegov@gmail.com>
2017-04-17 23:30:24 -0500

    Added tree printing

--- src/com/conversant/common/Throttling.py
+++ src/com/conversant/common/Throttling.py
@@ -2,27 +2,27 @@ from random import random
 
 
 class Throttling:
-    def __init__(self, cap, win_rate=0.03, period=5000):
+    def __init__(self, cap,  period=5000):
         self.value = 0.5
-        self.win_rate = win_rate
         self.cap = cap
         self.window = []
         self.results = []
         self.period = period
 
-    def tick(self, n_bids):
+    def tick(self, n_bids, win_rate=0.03):
         # simulate biddy/exchange/nessy
         n_impressions = 0
         for i in range(n_bids):
-            n_impressions += self.win_rate if random() <= self.value else 0
+            n_impressions += win_rate if random() <= self.value else 0
 
         # simulate maelstrom
-        self.window.insert(0, [n_impressions, n_impressions / self.value])
+        d = [n_impressions, n_impressions / self.value]
+        self.window.insert(0, d)
         if len(self.window) > self.period:
             self.window.pop()
 
-        # impression_stats[0] impressions over period
-        # impression_stats[1] available impressions over period
+        # [0] impressions over period
+        # [1] available impressions over period
         impression_stats = [sum(i) for i in zip(*self.window)]
 
         # throttle = (cap / available) x correction
@@ -31,7 +31,8 @@ class Throttling:
             # simulate DMA
             self.value = min(1, (self.cap / impression_stats[1])) * pow(min(1, self.cap / impression_stats[0]), 8)
 
-        self.results.append([impression_stats[0], self.cap, n_bids])
+        if len(self.window) == self.period:
+            self.results.append([impression_stats[0], self.cap, n_bids])
 
     def __del__(self):
         del self.window
--- test/unit/conversant/common/TrafficGeneratorTestCase.py
+++ test/unit/conversant/common/TrafficGeneratorTestCase.py
@@ -1,23 +1,32 @@
 import unittest
 from matplotlib import pyplot
+from random import random
 from com.conversant.common.TrafficGenerator import TrafficGenerator
 from com.conversant.common.Throttling import Throttling
 
 
 class TrafficGeneratorTestCase(unittest.TestCase):
     def setUp(self):
-        self.generator = TrafficGenerator(max_traffic=200, min_traffic=40, noise_to_signal=0.05)
-        self.throttle = Throttling(100, win_rate=0.03, period=3000)
+        self.generator = TrafficGenerator(max_traffic=100, min_traffic=10, noise_to_signal=0.05)
+        self.throttle = Throttling(100, period=2000)
 
     def tearDown(self):
         del self.generator
         del self.throttle
 
     def test_gen(self):
-        self.generator.gen_period(n_periods=4, tick_per_period=3000)
+        self.generator.gen_period(n_periods=10, tick_per_period=2000)
+
+        count = 0
+        win_rate = 0.01
 
         for i in self.generator.d:
-            self.throttle.tick(int(i))
+            count += 1
+            if count % (self.throttle.period / 2) == 0:
+                win_rate = win_rate * 1.5 if random() <= 0.5 else win_rate / 1.5
 
-        pyplot.plot(range(len(self.throttle.results)), self.throttle.results, marker='.')
+            self.throttle.tick(int(i), win_rate=win_rate)
+
+        plots = pyplot.plot(range(len(self.throttle.results)), self.throttle.results, marker='.')
+        pyplot.legend(iter(plots), ('spend', 'cap', 'opportunities'))
         pyplot.show()
